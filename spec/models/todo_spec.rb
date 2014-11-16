a63fee788eed1151426b27b10499852231541780
Turhan Coskun <turhancoskun@gmail.com>
2014-11-16 23:53:56 +0200

    Todo spec refactor edildi

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Todo, :type => :model do
   context 'is invalid without' do
-    
+    subject { Todo.new }    
     it 'a user' do
-      todo = Todo.new
-      todo.title = 'Kodlar refactor edilecek.'
-      expect(todo).to be_invalid
-      todo.user = User.new(email: 'foo@example.com')
-      expect(todo).to be_valid
+      subject.title = 'Kodlar refactor edilecek.'
+      expect(subject).to be_invalid
+      subject.user = fake_user 
+      expect(subject).to be_valid
     end
 
     it 'a title' do
-      todo = Todo.new
-      todo.title = nil
-      todo.user = User.new(email: 'foo@example.com')
-      expect(todo).to be_invalid
+      subject.title = nil
+      subject.user = fake_user
+      expect(subject).to be_invalid
+    end
+
+    def fake_user
+      User.new(email: 'foo@example.com')
     end
 
   end
